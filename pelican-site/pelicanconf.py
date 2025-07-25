c993b640df0bcf25450df9894d6a5bea133b97f1
Patrick Little <patrick.little@tbs-sct.gc.ca>
2025-07-25 15:45:04 -0400

    Update pelicanconf.py

--- pelican-site/pelicanconf.py
+++ pelican-site/pelicanconf.py
@@ -1,6 +1,6 @@
 SITENAME = 'Proactive Docs'
 SITEURL = 'https://patlittle.github.io/proactive-docs/pelican'
-PATH = 'content'
+PATH = '../docs'  # Points to the docs directory at the root of your repo
 TIMEZONE = 'UTC'
 DEFAULT_LANG = 'en'
 THEME = 'notmyidea'
