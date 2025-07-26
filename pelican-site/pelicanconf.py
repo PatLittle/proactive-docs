99b3948d0947f33195004f0aaf3caaea2966b69e
Patrick Little <patrick.little@tbs-sct.gc.ca>
2025-07-25 21:23:18 -0400

    Delete pelican-site directory

--- pelican-site/pelicanconf.py
+++
@@ -1,6 +0,0 @@
-SITENAME = 'Proactive Docs'
-SITEURL = 'https://patlittle.github.io/proactive-docs/pelican'
-PATH = '../docs'  # Points to the docs directory at the root of your repo
-TIMEZONE = 'UTC'
-DEFAULT_LANG = 'en'
-THEME = 'notmyidea'
